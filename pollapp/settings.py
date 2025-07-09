c4fec683fa77160dd3eb06cc6a08a283ef454ae1
Abisola <abipriscilla2007@gmail.com>
2025-07-09 11:21:48 +0100

    Added base template and fixed login redirect

--- pollapp/settings.py
+++ pollapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'pollapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'polls' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+LOGIN_URL = 'accounts/login'
 
 
 # Password validation
